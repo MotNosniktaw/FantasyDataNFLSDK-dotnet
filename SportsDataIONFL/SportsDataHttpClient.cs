f5f09d738fe1b75dd10d2e112f5105a9fd1845da
MotNosniktaw <tomwatkinson90gmail.com>
2021-08-03 18:57:41 +0100

    first commit

---
+++ SportsDataIONFL/SportsDataHttpClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SportsDataIONFL
+{
+    public class SportsDataHttpClient
+    {
+        private string apiKey;
+        private HttpClient httpClient;
+
+        public SportsDataHttpClient(string apiKey)
+        {
+            this.apiKey = apiKey;
+            this.httpClient = new HttpClient();
+            this.httpClient.BaseAddress = new Uri("https://sportsdata.io/v3/nfl");
+        }
+
+        public async Task<HttpResponseMessage> Get(string url)
+        {
+            var request = new HttpRequestMessage
+            {
+                RequestUri = new Uri(url),
+                Method = HttpMethod.Get,
+                Headers =
+                {
+                    { "Ocp-Apim-Subscription-Key", this.apiKey }
+                }
+            };
+
+            return await this.httpClient.SendAsync(request);
+        }
+    }
+}
