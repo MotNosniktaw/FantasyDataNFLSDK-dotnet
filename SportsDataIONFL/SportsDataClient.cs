a98e647de3a4a7928de0633cad51106c8dab967f
MotNosniktaw <tomwatkinson90gmail.com>
2021-08-03 19:07:03 +0100

    add skeleton client class

---
+++ SportsDataIONFL/SportsDataClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsDataIONFL
+{
+    class SportsDataClient
+    {
+        private SportsDataHttpClient httpClient;
+
+        public SportsDataClient(string apiKey)
+        {
+            this.httpClient = new SportsDataHttpClient(apiKey);
+        }
+
+        public async Task<HttpResponseMessage> GetPlayerDetails()
+        {
+            return await this.httpClient.Get("scores/json/players");
+        }
+    }
+}
